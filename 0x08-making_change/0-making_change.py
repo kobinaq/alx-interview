5151d9187bd0833cd7d7cad26ddba74757a777a4
kobinaq <kobinastudios@gmail.com>
2024-12-04 12:56:53 +0000

    Task 0

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,5 +1,20 @@
 #!/usr/bin/python3
 
+  """
+    Determine the fewest number of coins needed to meet a given total amount.
+
+    Parameters:
+    coins (list of int): A list of the values of the coins in your possession.
+                         Each coin value is a positive integer.
+    total (int): The target amount you want to reach using the fewest number of coins.
+
+    Returns:
+    int: The fewest number of coins needed to meet the total amount.
+         - Returns 0 if the total is 0 or less.
+         - Returns -1 if the total cannot be met by any combination of the coins.
+
+    """
+
 def makeChange(coins, total):
     if total <= 0:
         return 0
